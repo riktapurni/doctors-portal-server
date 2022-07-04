f9df7078af558d7062aa6bbd377113a59d8806f0
rikta das <riktapurni@gmail.com>
2022-07-04 20:18:33 +0600

    admin service key added

--- index.js
+++ index.js
@@ -3,19 +3,46 @@ const { MongoClient } = require('mongodb');
 const cors = require('cors');
 require('dotenv').config()
 const ObjectId = require('mongodb').ObjectId;
+// stripe payment 
 const stripe = require('stripe')(process.env.STRIPE_SECRET);
+// for image 
 const fileUpload = require('express-fileupload');
-
+//firebase admin sdk
+const admin = require("firebase-admin");
 const app = express();
+
 const port = process.env.PORT || 5000;
+// firebase service Account key
+const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 //Middleware
 app.use(cors());
 app.use(express.json());
-app.use(fileUpload())
+app.use(fileUpload());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gu8vt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 console.log(uri);
+
+async function verifyToken(req, res, next) {
+    if (req.headers?.authorization?.startsWith('Bearer ')) {
+        const token = req.headers.authorization.split(' ')[1];
+
+        try {
+            const decodedUser = await admin.auth().verifyIdToken(token);
+            req.decodedEmail = decodedUser.email;
+        }
+        catch {
+
+        }
+
+    }
+    next();
+}
+
     async function run(){
         try{
         await client.connect();
@@ -82,9 +109,10 @@ console.log(uri);
             res.json(result);
         });
         //make admin api
-        app.put('/users/admin', async(req, res) => {
+        app.put('/users/admin', verifyToken, async(req, res) => {
             const user = req.body;
-            console.log('put', req.headers)
+            // console.log('put', req.headers)
+            console.log('put', req.headers.authorization)
             const filter = {email:user.email};
             const updateDoc = { $set: {role:'admin'} };
             const result = await userCollection.updateOne(filter, updateDoc);
