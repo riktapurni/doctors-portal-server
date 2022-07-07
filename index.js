eae5bd728c4c1c02af34ab912c6bc9341c86a0b2
rikta das <riktapurni@gmail.com>
2022-07-07 21:19:15 +0600

    update

--- index.js
+++ index.js
@@ -10,11 +10,9 @@ const fileUpload = require('express-fileupload');
 //firebase admin sdk
 const admin = require("firebase-admin");
 const app = express();
-
 const port = process.env.PORT || 5000;
 // firebase service Account key
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
-
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount)
 });
@@ -23,7 +21,6 @@ app.use(cors());
 app.use(express.json());
 app.use(fileUpload());
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.gu8vt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
 console.log(uri);
 
