b1c6725cbfa2883f558f71330605368e16ebde00
rikta das <riktapurni@gmail.com>
2022-07-07 20:41:02 +0600

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ const fileUpload = require('express-fileupload');
 const admin = require("firebase-admin");
 const app = express();
 
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 3000;
 // firebase service Account key
 const serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
 
